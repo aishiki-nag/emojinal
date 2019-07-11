0ff4d978b8e7262150d47ef6a20986e030aa066b
nimet <bethkaratas@icloud.com>
2019-07-11 16:32:36 -0700

    First commit

---
+++ Emojinal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Emojinal
+//
+//  Created by Apple on 7/11/19.
+//  Copyright © 2019 Nimet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func Orchestra(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func Band(_ sender: UIButton) {
+    }
+    
+}
+
